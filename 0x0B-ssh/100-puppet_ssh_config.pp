4673c51687be13446aa26acb04d80140b3b42115
Onyibaybay <onyinyechukwuedith090@gmail.com>
2024-03-27 15:55:23 -0700

    100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# using puppet to configure file
+
+file { 'etc/ssh/ssh_config':
+	ensure => present.
+
+content =>"
+
+	#SSH client configuration
+	host*
+	identityFile ~/.ssh/school
+	PasswordAuthentication no
+
+}
